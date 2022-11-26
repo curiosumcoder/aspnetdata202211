8c248a0aca596642a6cd6392d1ee6bafa084ac15
Gilberto Bermúdez Garro <gbermude@outlook.com>
2022-11-25 20:07:46 -0600

    Demostraciones

---
+++ Sesion7/WA8/WA8/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WA8.Data;
+using WA8.Models;
+using WA8.ViewModels;
+
+namespace WA8.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly NWContext _db;
+
+        public HomeController(ILogger<HomeController> logger, NWContext db)
+        {
+            _logger = logger;
+            _db = db;
+        }
+
+        public IActionResult Index(HomeIndexViewModel vm)
+        {
+            //var data = new List<Product>();
+            //using (var db = new NWContext())
+            //{
+            //    data = db.Products.ToList();
+            //}
+
+            //return View(_db.Products.ToList());
+
+            if (!string.IsNullOrEmpty(vm.Filter))
+            {
+                vm.Products = _db.Products.Where(p => p.ProductName.Contains(vm.Filter)).ToList();
+            }
+
+            return View(vm);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Sesion7/WA8/WA8/ViewModels/HomeIndexViewModel.cs
@@ -0,0 +1,10 @@
+﻿using WA8.Models;
+
+namespace WA8.ViewModels
+{
+    public class HomeIndexViewModel
+    {
+        public string Filter { get; set; } = "";
+        public IList<Product> Products { get; set; } = new List<Product>();
+    }
+}
